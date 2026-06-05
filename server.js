0ecd53b0ab2b1dea6702c66a3cb2a878664f1543
zCrazMaex <822r5jtrtj@privaterelay.appleid.com>
2026-06-05 20:04:14 +0200

    Update server.js

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const DATA_FILE = path.join(__dirname, "data.json");
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "public"))); // ← NEU
 
 // Alle Teams + Fahrer abrufen
 app.get("/api/teams", (req, res) => {
